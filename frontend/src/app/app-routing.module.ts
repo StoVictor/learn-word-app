8e8f8fe3cd63922715e81d91cd82f4c075a85bd8
StoVictor <1vitek99@gmail.com>
2020-11-22 21:33:26 +0100

    Add auth handling

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TrainingAndGameComponent } from './training-and-game/training-and-game.component';
+import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth/auth.guard';
 
 
 const appRoutes: Routes = [
     { path: '', component: AppComponent },
-    { path: 'training_and_game', component: TrainingAndGameComponent }
+    { path: 'training_and_game', component: TrainingAndGameComponent, canActivate: [AuthGuard] },
+    { path: 'auth', component: AuthComponent }
 ]
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,32 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { TrainingAndGameComponent } from './training-and-game/training-and-game.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import { AuthComponent } from './auth/auth.component';
+import { AuthService } from './auth/auth.service';
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    TrainingAndGameComponent
+    TrainingAndGameComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
     MatToolbarModule,
     MatSidenavModule,
     MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [AuthService, {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
