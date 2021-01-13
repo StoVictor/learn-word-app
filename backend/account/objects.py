7090bd8fd1c7ba8e5f1b8eb39be862433eb59746
SergeyKapshuchenko <serg3y1999@gmail.com>
2021-01-13 21:41:01 +0200

    backend-models

--- backend/account/objects.py
+++ backend/account/objects.py
@@ -1,5 +1,7 @@
 from werkzeug.security import generate_password_hash
 
+from bson.objectid import ObjectId
+
 from backend.database.objects import ModelObjects
 
 from . import proxies
@@ -19,3 +21,19 @@ class UserObjects(ModelObjects):
                                               password=generate_password_hash(password),
                                               email=email)
         return user.save()
+
+
+class PackObjects(ModelObjects):
+    class Meta:
+        proxy = proxies.Pack
+
+    @classmethod
+    def create(cls, name: str, owner: ObjectId, from_language: str, to_language: str, public: bool = False) -> proxies.Pack:
+        pack = cls.Meta.proxy.create_instance(name=name,
+                                              owner=owner,
+                                              from_language=from_language,
+                                              to_language=to_language,
+                                              public=public,
+                                              words=[],
+                                              subscribed_packs=[])
+        return pack.save()
